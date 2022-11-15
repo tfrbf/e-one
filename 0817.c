8ba00a5e2f28b2ec978a463900688f866ff82c67
taha <amirtahafarshbaf@gmail.com>
2022-11-15 12:32:35 +0330

    first

---
+++ 0817.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+        do{
+                2+3
+        }
+
+
+
+
+        while()
+
+
+
+
+        while()
+        {
+2+3
+        }
+}
